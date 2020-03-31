7d90e959e731bf1c302d082518450a78df089f9e
MstfaZahr <mstfazahr@gmail.com>
2020-03-31 10:56:38 +0200

    Alpha 2
    - Added more sections.
    - DARK MODE STILL WORKS FLAWLESSLY!

--- app/assets/javascript.js
+++ app/assets/javascript.js
@@ -6,3 +6,17 @@ darkIcon.addEventListener("click", () => {
     body.classList.toggle("dark")
     darkIconBorder.classList.toggle("dark-enabled")
 })
+
+// --------------------------------------------------------
+
+const annualToggle = document.querySelector("#pricing svg")
+const annualToggleButton = annualToggle.querySelector(".toggle-button")
+const annualToggleBg = annualToggle.querySelectorAll(".toggle-bg")
+
+annualToggle.addEventListener("click", () => {
+    annualToggleButton.classList.toggle("annual-enabled-button")
+
+    for (Bg of annualToggleBg) {
+        Bg.classList.toggle("annual-enabled-bg")
+    }
+})
